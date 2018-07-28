27478925c5a9556f9dfae502122178c4a9b6fc1e
Unknown <theholyroger@theholyroger.com>
2018-07-28 01:48:21 +0100

    rogerd

--- BRPeer.c
+++ BRPeer.c
@@ -44,9 +44,9 @@
 #include <arpa/inet.h>
 
 #if BITCOIN_TESTNET
-#define MAGIC_NUMBER 0xf1c8d2fd
+#define MAGIC_NUMBER 0xf5c1d5ff
 #else
-#define MAGIC_NUMBER 0xdbb6c0fb
+#define MAGIC_NUMBER 0xebc4c2fc
 #endif
 #define HEADER_LENGTH      24
 #define MAX_MSG_LENGTH     0x02000000
